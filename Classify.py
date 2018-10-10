aeaeabc506b0517bd2bb360701a08157dee3308f
everestwitman <everesttwitman@gmail.com>
2018-10-10 09:34:36 -0400

    Del6 step 21 working classify.py, not working del2.py

--- Classify.py
+++ Classify.py
@@ -9,31 +9,39 @@ train4 = pickle.load(f)
 f = open('userData/train5.dat', 'rb')
 train5 = pickle.load(f)
 
+f = open('userData/train6.dat', 'rb')
+train6 = pickle.load(f)
+
 f = open('userData/test4.dat', 'rb')
 test4 = pickle.load(f)
 
 f = open('userData/test5.dat', 'rb')
 test5 = pickle.load(f)
 
+f = open('userData/test6.dat', 'rb')
+test6 = pickle.load(f)
+
 print train4.shape
 print train5.shape
 print test4.shape
 print test5.shape
 
-def ReshapeData(set1, set2):
-    X = np.zeros((2000, 5*4*6), dtype='f')
-    y = [] * 2000
+def ReshapeData(set1, set2, set3):
+    X = np.zeros((3000, 5*4*6), dtype='f')
+    y = [] * 3000
     for i in range(0, 1000):
         n = 0
         for j in range(0, 5):
             for k in range(0, 2):
                 for m in range(0, 3):
                     X[i, n] = set1[j, k, m, i]
-                    X[i + 1000, n] = set2[j, k, m, i]        
+                    X[i + 1000, n] = set2[j, k, m, i]   
+                    X[i + 2000, n] = set3[j, k, m, i]      
                     n = n + 1
                     
     y[0 : 999] = [4] * 1000;
     y[1000 : 1999] = [5] * 1000;
+    y[2000 : 2999] = [6] * 1000;
     
     return X, y
 
@@ -70,20 +78,24 @@ train4 = ReduceData(train4)
 train5 = ReduceData(train5)
 test4 = ReduceData(test4)
 test5 = ReduceData(test5)
+train6 = ReduceData(train6)
+test6 = ReduceData(test6)
 
 # Center data 
 train4 = CenterData(train4)
 train5 = CenterData(train5)
 test4 = CenterData(test4)
 test5 = CenterData(test5)
+train6 = CenterData(train6)
+test6 = CenterData(test6)
 
-reshapedTrainData = ReshapeData(train4, train5) 
+reshapedTrainData = ReshapeData(train4, train5, train6) 
 trainX = reshapedTrainData[0]
 trainy = reshapedTrainData[1]
 print trainX
 print trainy
 
-reshapedTestData = ReshapeData(test4, test5)
+reshapedTestData = ReshapeData(test4, test5, test6)
 testX = reshapedTestData[0]
 testy = reshapedTestData[1]
 print testX
---
+++ Del2Step11.py
@@ -0,0 +1,89 @@
+from Leap import * 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import random 
+import globalVariables as gv
+import pickle
+import numpy as np
+from sklearn import neighbors, datasets
+
+clf = pickle.load(open('userData/classifier.p', 'rb'))
+testData = np.zeros((1, 30), dtype='f')
+
+controller = Controller()
+lines = []
+
+matplotlib.interactive(True)
+fig = plt.figure(figsize = (8, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim(-300, 300)
+ax.set_ylim(-50, 250)
+ax.set_zlim(50, 500)
+ax.view_init(azim=90)
+
+def CenterData(X): 
+    print X
+    # Center X coordinates
+    allXCoordinates = X[0, ::3]
+    meanXValue = allXCoordinates.mean()
+    X[0, ::3] = allXCoordinates - meanXValue
+    
+    # Center Y coordinates
+    allYCoordinates = X[0, 1::3]
+    meanYValue = allYCoordinates.mean()
+    X[0, 1::3] = allYCoordinates - meanYValue
+    
+    # Center Z coordinates
+    allZCoordinates = X[0, 2::3]
+    meanZValue = allZCoordinates.mean()
+    X[0, 2::3] = allZCoordinates - meanZValue
+    
+    return X
+
+while True:
+    frame = controller.frame()
+    
+    
+    while (lines): 
+        ln = lines.pop()
+        ln.pop(0).remove()
+        del ln
+        ln = []
+        
+    # if at least one hand is in the frame 
+    if (len(frame.hands) > 0):
+        hand = frame.hands[0]
+        k = 0
+        for i in range(0, 5): 
+            finger = hand.fingers[i]
+            
+            for j in range(0, 4): 
+                bone = finger.bone(j)
+                boneBase = bone.prev_joint
+                boneTip = bone.next_joint
+
+                xBase = boneBase[0]
+                yBase = boneBase[1]
+                zBase = boneBase[2]
+                xTip = boneTip[0]
+                yTip = boneTip[1]
+                zTip = boneTip[2]
+                
+                lines.append(ax.plot([-xBase, -xTip], [zBase, zTip], [yBase, yTip], 'r'))
+                
+                if ( (j == 0) | (j == 3)):
+                    testData[0, k] = xTip
+                    testData[0, k + 1] = yTip
+                    testData[0, k + 2] = zTip
+                    k = k + 3
+                    
+        
+        print testData.shape
+        print clf
+        # testData = CenterData(testData)
+        predictedClass = clf.predict(testData)
+        print predictedClass
+    plt.pause(0.00001)
+    
+    
